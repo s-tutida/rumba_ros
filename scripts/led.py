957d2c5c1cc638ac076631a18b600dccc8ae6d0e
Shinya Tsuchida <u085195a@ecs.osaka-u.ac.jp>
2017-05-29 11:45:47 +0000

    make subscriber

--- scripts/led.py
+++ scripts/led.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python
 import rospy
+from std_msgs.msg import UInt16
+import RPi.GPIO as GPIO
+import time
 
-rospy.init_node('led_node')
-rospy.spin()
+def write_light(hz=0):
+#def write_light(mode=1):
+#	GPIO.setmode(GPIO.BCM)
+#	GPIO.setup(18,GPIO.OUT)
+#	GPIO.output(18,int(mode))
+#	time.sleep(1)
+#	GPIO.output(18,1-int(mode))
+#	GPIO.cleanup()
+#	rospy.loginfo(mode)
+        gpiofile = "/dev/rtbuzzer0"
+	try:
+		with open(gpiofile,"w") as f:
+			f.write(str(hz) + "\n" )
+			rospy.loginfo(data.data)
+	except IOError:
+		rospy.logerr("cant't write to" + gpiofile)
+
+def recv_light(data):
+	write_light(data.data)
+	
+if __name__ == '__main__':
+	rospy.init_node('led_node')
+        #when recieve messege from other node, call method of third option 
+	rospy.Subscriber("led_node", UInt16, recv_light)
+	rospy.spin()
 
--- test/test_led.py
+++ test/test_led.py
@@ -2,11 +2,22 @@
 import rospy,unittest,rostest
 import rosnode
 import time
+from std_msgs.msg import UInt16
 
 class ledTest(unittest.TestCase):
 	def test_node_exist(self):
 		nodes = rosnode.get_node_names()
 		self.assertIn('/led_node',nodes,"node doesnt exist")
+	
+	def test_put_value(self):
+		pub = rospy.Publisher('/led_node',UInt16)
+		for i in range(10)
+			pub.publish(1234)
+			time.sleep(0.1)
+		with open("/dev/rtbuzzer0","r") as f:
+			data = f.readline()
+			self.assertEqual(data,"1235\n","value does not written to rtbuzzer0")
+
 
 if __name__ == '__main__':
 	time.sleep(3)
