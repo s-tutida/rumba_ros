12a265e7162c40896166e59ee5556933d1ef9bd9
Shinya Tsuchida <u085195a@ecs.osaka-u.ac.jp>
2017-05-30 10:40:11 +0000

    create publisher

---
+++ scripts/led_pub.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import sys,rospy
+from rumba_ros.msg import GpioValue
+
+def get_freq():
+	f = rospy.get_param('led_pub_freq',10)
+	try:
+		if f <= 0.0:
+			raise Exception()
+		rospy.loginfo(f)
+	except:
+		rospy.logerr("value error: led_pub_freq")
+		sys.exit(1)
+	return f
+
+
+if __name__ == '__main__':
+#	devfile = '/dev/rtlightsensor0'
+	devfile = '/dev/test.txt'
+	rospy.init_node('led_pub_node')
+	pub = rospy.Publisher('led_pub_node',GpioValue,queue_size = 1)
+
+	freq = get_freq()
+	rate = rospy.Rate(freq)
+	while not rospy.is_shutdown():
+		try:
+			with open(devfile,'r') as f:
+				data = f.readline().split()
+				data = [int(e) for e in data]
+				d = GpioValue()
+				d.right_forward = data[0]
+				d.right_side = data[1]
+				d.left_side = data[2]
+				d.left_forward = data[3]
+				d.sum_all = sum(data)
+				d.sum_forward = data[0] + data[3]
+				pub.publish(d)
+		except IOError:
+			rospy.logerr("cannot read to " + devfile)
+
+		
+		f = get_freq()
+		if f != freq:
+			freq = f
+			rate = rospy.Rate(freq)
+
+
+		rate.sleep()
---
+++ test/test_led_pub.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import rospy,unittest,rostest
+import rosnode
+import time
+from rumba_ros.msg import GpioValue
+
+class ledTest(unittest.TestCase):
+	def setUp(self):
+		self.conf = 0
+		rospy.Subscriber('/led_node_pub',GpioValue,self.callback)
+
+	def callback(self,data):
+		self.count += 1
+		self.value = data
+
+	def check_values(self,lf,ls,rf)
+		vs = self.values
+		self.assertEqual(vs.left_forward, lf , "different value: left_forward")
+		self.assertEqual(vs.left_side, ls , "different value: left_side")
+		self.assertEqual(vs.right_forward, rf , "different value: right_forward")
+		self.assertEqual(vs.right_side, rs , "different value: right_side")
+		self.assertEqual(vs.sum_all, lf+ls+rf+rs , "different value: sum_all")
+		self.assertEqual(vs.sum_forward, lf+rf , "different value: sum_forward")
+	
+	def test_node_exist(self):
+		nodes = rosnode.get_node_names()
+		self.assertIn('/led_node_pub',nodes,"node doesnt exist")
+	
+	def test_get_value(self):
+		rospy.set_param('led_node_freq',10)
+		time.sleep(2)
+		with open("/dev/rtlightsensors0","w") as f:
+			f.write("-1 0 123 4321\n")
+
+		time.sleep(3)
+		self.assertFalse(self.count == 0,"cannot subscribe the topic")
+		self.check_values(4321,123,0,1)
+	
+	def test_change_paramater(self):
+		rospy.set_param('led_node_freq',1)
+		time.sleep(2)
+		c_prev = self.count
+		time.sleep(3)
+
+		self.assertTrue(self.count < c_prev + 4,"freq does not change")i
+		self.assertFalse(self.count == c_prev,"Subscriber is stopped")
+
+if __name__ == '__main__':
+	time.sleep(3)
+	rospy.init_node('led_test_node')
+	rostest.rosrun('rumba_ros','led_test_node',ledTest)
