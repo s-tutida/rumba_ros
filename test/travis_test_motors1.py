cc8038607d828bc3813058fc47ccaa14487792ff
Shinya Tsuchida <u085195a@ecs.osaka-u.ac.jp>
2017-05-30 13:05:30 +0000

    motor error 4

--- test/travis_test_motors1.py
+++ test/travis_test_motors1.py
@@ -46,7 +46,7 @@ class MotorTest(unittest.TestCase):
 if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_motors')
-    rostest.rosrun('pimouse_ros','travis_test_motors', MotorTest)
+    rostest.rosrun('rumba_ros','travis_test_motors', MotorTest)
 
 # Copyright 2016 Ryuichi Ueda
 # Released under the BSD License.
