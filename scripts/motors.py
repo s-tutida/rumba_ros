ec92441245e0960e36a04d2ba99c444a4d0ff5f8
Shinya Tsuchida <u085195a@ecs.osaka-u.ac.jp>
2017-05-31 05:17:16 +0000

    add message

--- scripts/motors.py
+++ scripts/motors.py
@@ -76,7 +76,7 @@ class Motor():
 	dev = "/dev/rtmotor0"
 	try:
 		with open(dev,'w') as f:
-			f.write("%d %d %d \n" % (message.left_hz,message.right_hz,duration_ms))
+			f.write("%d %d %d \n" % (message.left_hz,message.right_hz,message.duration_ms))
 	except:
 		rospy.logerr("cannot write to" + dev)
 		return False
