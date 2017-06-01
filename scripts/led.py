903785c5504e6b28153985186614127091087b9e
Shinya Tsuchida <u085195a@ecs.osaka-u.ac.jp>
2017-06-01 08:22:01 +0000

    error typing

--- scripts/led.py
+++ scripts/led.py
@@ -2,29 +2,15 @@
 import rospy,actionlib
 from std_msgs.msg import UInt16
 from rumba_ros.msg import MusicAction,MusicResult,MusicFeedback
-#import RPi.GPIO as GPIO
-#import time
 
-def write_light(hz=0):
-#def write_light(mode=1):
-#	GPIO.setmode(GPIO.BCM)
-#	GPIO.setup(18,GPIO.OUT)
-#	GPIO.output(18,int(mode))
-#	time.sleep(1)
-#	GPIO.output(18,1-int(mode))
-#	GPIO.cleanup()
-#	rospy.loginfo(mode)
+def write_freq(hz=0):
         gpiofile = "/dev/rtbuzzer0"
 	try:
 		with open(gpiofile,"w") as f:
 			f.write(str(hz) + "\n" )
-			rospy.loginfo(str(hz))
 	except IOError:
 		rospy.logerr("cant't write to" + gpiofile)
 
-def recv_light(data):
-	write_light(data.data)
-	
 def exec_music(goal):
 	r= MusicResult()
 	fb= MusicFeedback()
@@ -34,15 +20,18 @@ def exec_music(goal):
 		music.publish_feedback(fb)
 
 		if music.is_preempt_requested():
-			write_light(0)
+			write_freq(0)
 			r.finished = False
-			music.set_parameter(r)
+			music.set_preempted(r)
 			return
-		write_light(f)
+		write_freq(f)
 		rospy.sleep(1.0 if i >= len(goal.durations) else goal.durations[i])
 
 	r.finished = True
 	music.set_succeeded(r)
+
+def recv_light(data):
+	write_freq(data.data)
 
 if __name__ == '__main__':
 	rospy.init_node('led_node')
@@ -50,6 +39,6 @@ if __name__ == '__main__':
 	rospy.Subscriber("led_node", UInt16, recv_light)
 	music = actionlib.SimpleActionServer('music',MusicAction,exec_music,False)
 	music.start()
-	rospy.on_shutdown(write_light)
+	rospy.on_shutdown(write_freq)
 	rospy.spin()
 
